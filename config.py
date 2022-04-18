5116b2c426fc875ff8f05d455b5725c22249b0b4
Mrestof <fotserm@gmail.com>
2022-04-19 00:03:55 +0300

    make configs easier to generate and edit

--- config.py
+++ config.py
@@ -1,24 +1,37 @@
-class Base:
-    port = 5867
+from configparser import ConfigParser
+
+CFG_FILENAME = 'config.ini'
 
 
-class Test(Base):
-    listen_address = ''
-    data_rows = (
-        b'first response',
-        b'second response',
-        b'third response',
-    )
-    allowed_hosts = (
-        '127.0.0.1',
-    )
+def get_configs(section: str) -> dict[str, int | str | list[str]]:
+    config_dict = dict()
+    necessary_params = {
+        'port': int,
+        'listen_address': str,
+        'data_rows': lambda s: map(str.encode, s.split('\n')),
+        'allowed_hosts': lambda s: s.split('\n'),
+    }
+
+    config = ConfigParser()
+    config.read(CFG_FILENAME)
+
+    try:
+        config_params = config[section]
+    except KeyError:
+        print(f'!!section {section} was not found in {CFG_FILENAME}')
+        return {}
+
+    try:
+        for param_name, param_translator in necessary_params.items():
+            config_dict[param_name] = param_translator(config_params[param_name])
+    except KeyError as ke:
+        print(f'!!necessary param {ke} in section {section} was not found in {CFG_FILENAME}')
+        return {}
+    except Exception as e:
+        print(f'!!some unexpected error happened, details: {e}')
+
+    return config_dict
 
 
-class Prod(Base):
-    listen_address = ''
-    data_rows = (
-        '',
-    )
-    allowed_hosts = (
-        '',
-    )
+if __name__ == '__main__':
+    print(get_configs('test'))
--- server.py
+++ server.py
@@ -2,22 +2,25 @@
 # TODO: replace print with varying levels of logging
 # TODO: structure the code a bit more fine
 import socket
-from config import Test as Config
+from config import get_configs
+
+config = get_configs('test')
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as listen_socket:
-    listen_socket.bind((Config.listen_address, Config.port))
-    print(f'##listen on {Config.listen_address}:{Config.port}')
+    listen_socket.bind((config['listen_address'], config['port']))
+    print(f'##listen on {config["listen_address"]}:{config["port"]}')
     listen_socket.listen()
-    for response_row in Config.data_rows:
+    for response_row in config['data_rows']:
         print(f'##prepare next response row to reply with: {response_row = }')
         while True:
             print('..wait for incoming connections')
             client_socket, client_address = listen_socket.accept()
             print(f'<-catch new connection from {client_address}')
             with client_socket:
-                if client_address[0] in Config.allowed_hosts:
+                if client_address[0] in config['allowed_hosts']:
                     client_socket.sendall(response_row)
                     print(f'->send the response row to the client')
                     break
                 # else
                 print('!!reject connection from host not in whitelist')
+    print('##finish, stop the server, close the listening socket')
