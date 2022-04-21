f57d89eab1003d363881cab5092c7373f6e6d6bb
Mrestof <fotserm@gmail.com>
2022-04-22 00:57:23 +0300

    fix behaviour to emulate `104` more correctly
    Here, `104` is an alias for IEC 60870-5-104 protocol.

--- server.py
+++ server.py
@@ -10,17 +10,20 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as listen_socket:
     listen_socket.bind((config['listen_address'], config['port']))
     print(f'##listen on {config["listen_address"]}:{config["port"]}')
     listen_socket.listen()
-    for response_row in config['data_rows']:
-        print(f'##prepare next response row to reply with: {response_row = }')
-        while True:
-            print('..wait for incoming connections')
-            client_socket, client_address = listen_socket.accept()
-            print(f'<-catch new connection from {client_address}')
-            with client_socket:
-                if client_address[0] in config['allowed_hosts']:
-                    client_socket.sendall(response_row)
-                    print(f'->send the response row to the client')
-                    break
-                # else
+    while True:
+        print('..wait for incoming connections')
+        client_socket, client_address = listen_socket.accept()
+        print(f'<-catch new connection from {client_address}')
+        with client_socket:
+            if client_address[0] not in config['allowed_hosts']:
                 print('!!reject connection from host not in whitelist')
-    print('##finish, stop the server, close the listening socket')
+            else:
+                for response_row in config['data_rows']:
+                    print('..listen for data from current connection')
+                    recv_data = client_socket.recv(1024)  # or lower\higher?
+                    print(f'<-{recv_data} received from the current connection')
+                    # TODO: do some checks and transformation to the data sent back
+                    client_socket.sendall(response_row)  # might throw the errno32 broken pipe
+                    print(f'->{response_row=} sent to the client')
+                break
+    print('##no data left, stop the server, close the listening socket')
